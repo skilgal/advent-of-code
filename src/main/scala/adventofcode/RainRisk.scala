ea19f41f05dc20492eded042c57a04f791477247
Dmytro Altsyvanovych <dmytro.altsyvanovych@exabeam.com>
2020-12-27 02:26:48 +0200

    Added Day 12. Rain Risk 2

--- src/main/scala/adventofcode/RainRisk.scala
+++ src/main/scala/adventofcode/RainRisk.scala
@@ -1,13 +1,26 @@
 package adventofcode
 
+import javax.swing.text.Position
+import scala.annotation.tailrec
+
 object RainRisk extends App with ProgramWithSource {
 
+  // override def filename: String = "test.txt"
   override def filename: String = "rain-risk.txt"
 
-  case class Position(east: Int, south: Int)
-  implicit class Addicter(pos: Position) {
+  case class Position(east: Int, north: Int)
+
+  implicit class PositionOps(pos: Position) {
     def +(diff: Position): Position = {
-      Position(pos.east + diff.east, pos.south + diff.south)
+      Position(pos.east + diff.east, pos.north + diff.north)
+    }
+
+    def -(endPos: Position): Position = {
+      Position(endPos.east - pos.east, endPos.north - pos.north)
+    }
+
+    def *(count: Int): Position = {
+      Position(pos.east * count, pos.north * count)
     }
   }
 
@@ -30,68 +43,61 @@ object RainRisk extends App with ProgramWithSource {
   }
   import WorldSide._
 
-  val stepPattern = """(\w)(\d+)""".r
+  def rotate(point: Position, times: Int): Position = times match {
+    // Error could be here
+    case times if times % 4 == 0 => point
+    case times if times % 4 == 1 => Position(point.north, -point.east)
+    case times if times % 4 == 2 => Position(-point.east, -point.north)
+    case times if times % 4 == 3 => Position(-point.north, point.east)
+    case times if times < 0      => rotate(point, 4 + times)
+  }
 
-  def rotate(facing: WorldSide, times: Int): WorldSide = {
-    if (times == 0) facing
-    else
-      facing match {
-        case WorldSide.East =>
-          if (times > 0) rotate(South, times - 1)
-          else rotate(North, times + 1)
-        case West =>
-          if (times > 0) rotate(North, times - 1)
-          else rotate(South, times + 1)
-        case North =>
-          if (times > 0) rotate(East, times - 1)
-          else rotate(West, times + 1)
-        case South =>
-          if (times > 0) rotate(West, times - 1)
-          else rotate(East, times + 1)
-      }
+  def time[T](name: String, op: => T): T = {
+    val time = System.currentTimeMillis()
+    val res = op
+    println(s"Operation: ${name} took ${System.currentTimeMillis() - time}")
+
+    res
   }
 
   def stepTo(direction: WorldSide, steps: Int): Position = direction match {
     case East  => Position(steps, 0)
     case West  => Position(-steps, 0)
-    case North => Position(0, -steps)
-    case South => Position(0, steps)
+    case North => Position(0, steps)
+    case South => Position(0, -steps)
   }
 
   var facingSide: WorldSide = WorldSide.East
-  var position = Position(10, -1)
+  var shipPosition = Position(0, 0)
+  var wayPoint = Position(10, 1)
 
-  // move("N1")
-  // move("S1")
-  // move("E1")
-  // move("W1")
-  // move("F1")
-  // move("R180")
-  // move("F2")
-  // move("L180")
-  // move("F1")
-  // println(position)
-
-  // System.exit(0)
-
-  def move: PartialFunction[String, Unit] = { case stepPattern(direction, stepCountStr) =>
-    val stepCount = Integer.parseInt(stepCountStr)
+  def move(line: String): Unit = {
+    val direction = line.head
+    val stepCount = Integer.parseInt(line.tail)
 
     direction match {
-      case "N" => position += stepTo(North, stepCount)
-      case "S" => position += stepTo(South, stepCount)
-      case "E" => position += stepTo(East, stepCount)
-      case "W" => position += stepTo(West, stepCount)
-      case "L" => facingSide = rotate(facingSide, -stepCount / 90)
-      case "R" => facingSide = rotate(facingSide, stepCount / 90)
-      case "F" => position += stepTo(facingSide, stepCount)
+      case 'N' => wayPoint += stepTo(North, stepCount)
+      case 'S' => wayPoint += stepTo(South, stepCount)
+      case 'E' => wayPoint += stepTo(East, stepCount)
+      case 'W' => wayPoint += stepTo(West, stepCount)
+      case 'L' => wayPoint = rotate(wayPoint, -stepCount / 90)
+      case 'R' => wayPoint = rotate(wayPoint, stepCount / 90)
+      case 'F' =>
+        for (_ <- 1 to stepCount) {
+          shipPosition += wayPoint
+        }
     }
+
+    // println(s"command: ${line}")
+    // println(s"Ship position: ${shipPosition}")
+    // println(s"Waypoint ${wayPoint}")
   }
 
   fileSource.toList.map(move)
 
   println(
-    s"Final position is ${position.east} + ${position.south} = ${Math.abs(position.east) + Math.abs(position.south)}"
+    s"Final position is ${shipPosition.east} + ${shipPosition.north} = ${Math
+      .abs(shipPosition.east) + Math.abs(shipPosition.north)}"
   )
 
 }
